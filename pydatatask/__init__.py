9dca2c95c7d57f5920dc1eb031631170e72cbd28
Audrey Dutcher <audrey@rhelmot.io>
2022-12-11 06:58:12 -0700

    attempt ci/cd

--- pydatatask/__init__.py
+++ pydatatask/__init__.py
@@ -1,8 +1,9 @@
-__version__ = "0.1.0"
-
 from .main import *
 from .pipeline import *
 from .pod_manager import *
 from .repository import *
 from .session import *
 from .task import *
+
+__version__ = "0.0.1-dev.2"
+released_version = "0.0.0"
