62f88ebeb694491d2766094bec95816c258d7ccf
Audrey Dutcher <audrey@rhelmot.io>
2022-12-11 16:40:58 -0700

    Release 0.1.4

--- pydatatask/__init__.py
+++ pydatatask/__init__.py
@@ -5,5 +5,5 @@ from .repository import *
 from .session import *
 from .task import *
 
-__version__ = "0.1.4-dev.1"
-released_version = "0.1.3"
+__version__ = "0.1.5-dev.1"
+released_version = "0.1.4"
