1202675c893d87348e40690ab43e18ae92def6aa
Wil Gibbs <wfgibbs@asu.edu>
2023-11-06 19:27:43 -0700

    linting

--- pydatatask/visualize.py
+++ pydatatask/visualize.py
@@ -2,7 +2,7 @@ import networkx as nx
 import tempfile
 import asyncio
 
-#from queue import Queue
+# from queue import Queue
 from typing import Tuple
 from concurrent.futures import ThreadPoolExecutor
 from multiprocessing import Process, Queue
@@ -16,11 +16,10 @@ from dash import dcc, html
 from dash.dependencies import Input, Output
 
 
-
 app = dash.Dash("pydatatask")
 
-class TaskVisualizer:
 
+class TaskVisualizer:
     def __init__(self, pipeline):
         self.pipeline = pipeline
         self.status_colors = {
@@ -31,45 +30,40 @@ class TaskVisualizer:
         }
         self.nodes = {}
         self.register_callbacks()
-    
+
     def left_to_right_layout(self, G, ranksep=0.1):
         A = nx.nx_agraph.to_agraph(G)
-        A.layout(prog='dot', args=f'-Grankdir=LR -Granksep={ranksep}')  # Left to Right layout
-        pos = nx.nx_agraph.graphviz_layout(G, prog='dot', args=f'-Grankdir=LR -Granksep={ranksep}')
+        A.layout(prog="dot", args=f"-Grankdir=LR -Granksep={ranksep}")  # Left to Right layout
+        pos = nx.nx_agraph.graphviz_layout(G, prog="dot", args=f"-Grankdir=LR -Granksep={ranksep}")
         return pos
-
 
     @staticmethod
     def generate_layout():
-        graph_layout = dcc.Graph(id='network-graph',
-                                 style={"width": "100%", "height": "90vh"},
-                                 config={'doubleClick': 'reset+autosize'})
-        interval_layout = dcc.Interval(id='interval-component',
-                                       interval=5 * 1000,  # in milliseconds
-                                       n_intervals=0)
-        style = {'width': '100%', 'height': '90vh', 'margin': '0', 'padding': '0'}
+        graph_layout = dcc.Graph(
+            id="network-graph", style={"width": "100%", "height": "90vh"}, config={"doubleClick": "reset+autosize"}
+        )
+        interval_layout = dcc.Interval(id="interval-component", interval=5 * 1000, n_intervals=0)  # in milliseconds
+        style = {"width": "100%", "height": "90vh", "margin": "0", "padding": "0"}
 
         return html.Div([graph_layout, interval_layout], style=style)
-
 
     def create_rectangle_shapes(self, pos, width=60, height=30):
         shapes = []
         for _, (x, y) in pos.items():
             shape = {
-                'type': 'rect',
-                'x0': x - width/2,
-                'y0': y - height/2,
-                'x1': x + width/2,
-                'y1': y + height/2,
-                'line': {
-                    'color': 'blue',
-                    'width': 2,
+                "type": "rect",
+                "x0": x - width / 2,
+                "y0": y - height / 2,
+                "x1": x + width / 2,
+                "y1": y + height / 2,
+                "line": {
+                    "color": "blue",
+                    "width": 2,
                 },
-                'fillcolor': 'blue'
+                "fillcolor": "blue",
             }
             shapes.append(shape)
         return shapes
-
 
     def create_quiver_plot(self, G, pos):
         edge_x = []
@@ -85,13 +79,8 @@ class TaskVisualizer:
             dx.append(x1 - x0)
             dy.append(y1 - y0)
 
-        quiver = ff.create_quiver(edge_x, edge_y, dx, dy,
-                                scale=1,
-                                arrow_scale=0.1,
-                                line=dict(width=0.8, color='#888'))
+        quiver = ff.create_quiver(edge_x, edge_y, dx, dy, scale=1, arrow_scale=0.1, line=dict(width=0.8, color="#888"))
         return quiver.data[0]
-
-
 
     async def get_live_and_done(self, node):
         repo_count = {}
@@ -108,7 +97,7 @@ class TaskVisualizer:
         queue.put(result)
 
     def sync_function(self, node):
-        if node in self.nodes and 'done' in self.nodes[node] and self.nodes[node]['done'] > 0:
+        if node in self.nodes and "done" in self.nodes[node] and self.nodes[node]["done"] > 0:
             return self.nodes[node]
 
         queue = Queue()
@@ -121,27 +110,23 @@ class TaskVisualizer:
 
         self.nodes[node] = result
         return result
-        
+
     def register_callbacks(self):
-        @app.callback(
-            Output('network-graph', 'figure'),
-            [Input('interval-component', 'n_intervals')]
-        )
+        @app.callback(Output("network-graph", "figure"), [Input("interval-component", "n_intervals")])
         def update_graph(n):
             pl = self.pipeline
             new_graph = pl.task_graph
             pos = self.left_to_right_layout(new_graph)
 
-
             annotations = []
             for node, (x, y) in pos.items():
                 results = self.sync_function(node)
-                if 'done' not in results:
+                if "done" not in results:
                     print(node, results)
                 if results is not None:
-                    if results['live'] > 0:
+                    if results["live"] > 0:
                         node_color = self.status_colors["running"]
-                    elif results['done'] > 0:
+                    elif results["done"] > 0:
                         node_color = self.status_colors["success"]
                     else:
                         node_color = self.status_colors["pending"]
@@ -161,7 +146,7 @@ class TaskVisualizer:
                         bordercolor="black",
                         borderwidth=2,
                         borderpad=4,
-                        hovertext='<br>'.join(f"{k}:{v}" for k, v in self.nodes[node].items())
+                        hovertext="<br>".join(f"{k}:{v}" for k, v in self.nodes[node].items()),
                     )
                 )
 
@@ -173,54 +158,55 @@ class TaskVisualizer:
                 x0, y0 = pos[edge[0]]
                 x1, y1 = pos[edge[1]]
 
-                if edge[0] in self.nodes and self.nodes[edge[0]]['done'] > 0:
+                if edge[0] in self.nodes and self.nodes[edge[0]]["done"] > 0:
                     done_edge_x.extend([x0, x1, None])
                     done_edge_y.extend([y0, y1, None])
                 else:
                     edge_x.extend([x0, x1, None])
                     edge_y.extend([y0, y1, None])
 
-
             fig = go.Figure()
 
-            #fig.add_trace(create_quiver_plot(new_graph, pos))
-            fig.add_trace(go.Scatter(
-                x=edge_x, y=edge_y,
-                line=dict(width=1, color='#888'),
-                mode='lines'))
+            # fig.add_trace(create_quiver_plot(new_graph, pos))
+            fig.add_trace(go.Scatter(x=edge_x, y=edge_y, line=dict(width=1, color="#888"), mode="lines"))
 
-            fig.add_trace(go.Scatter(
-                x=done_edge_x, y=done_edge_y,
-                line=dict(width=1, color=self.status_colors["success"]),
-                mode='lines'))
-
+            fig.add_trace(
+                go.Scatter(
+                    x=done_edge_x, y=done_edge_y, line=dict(width=1, color=self.status_colors["success"]), mode="lines"
+                )
+            )
 
             node_x = [pos[node][0] for node in new_graph.nodes()]
             node_y = [pos[node][1] for node in new_graph.nodes()]
 
-            fig.add_trace(go.Scatter(
-                x=node_x, y=node_y,
-                mode='markers',
-            ))
+            fig.add_trace(
+                go.Scatter(
+                    x=node_x,
+                    y=node_y,
+                    mode="markers",
+                )
+            )
 
             fig.update_layout(
-                title='pydatatask visualizer',
+                title="pydatatask visualizer",
                 showlegend=False,
-                #uirevision='some-constant-value',
+                # uirevision='some-constant-value',
                 margin=dict(b=0, l=0, r=0, t=40),
                 xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
                 yaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
                 hoverlabel=dict(font_color="white"),
-                annotations=annotations
+                annotations=annotations,
             )
 
             return fig
+
 
 def run_viz(pipeline):
     tv = TaskVisualizer(pipeline)
     app.run_server(debug=True)
 
+
 app.layout = TaskVisualizer.generate_layout()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
