1ff7e002e50b20d986b17bdb450e59e5b6d7b098
Nicola Ruaro <ruaronicola@gmail.com>
2023-11-22 22:14:35 +0000

    add docker network options - default to 'host'

--- pydatatask/task.py
+++ pydatatask/task.py
@@ -1613,6 +1613,7 @@ class ContainerTask(Task):
         done: Optional["repomodule.MetadataRepository"] = None,
         logs: Optional["repomodule.BlobRepository"] = None,
         ready: Optional["repomodule.Repository"] = None,
+        network: str = 'host',
         privileged: Optional[bool] = False,
         tty: Optional[bool] = False,
     ):
@@ -1655,6 +1656,7 @@ class ContainerTask(Task):
         self._manager: Optional[execmodule.AbstractContainerManager] = None
         self.warned = False
         self.window = window
+        self.network = network
         self.privileged = privileged
         self.tty = tty
         self.mount_directives: DefaultDict[str, List[Tuple[str, str]]] = defaultdict(list)
@@ -1739,6 +1741,7 @@ class ContainerTask(Task):
 
         mounts = self.mount_directives.pop(job, [])
 
+        network = self.network
         privileged = bool(self.privileged)
         tty = bool(self.tty)
         await self.manager.launch(
@@ -1750,6 +1753,7 @@ class ContainerTask(Task):
             self.environ,
             self.job_quota,
             mounts,
+            network,
             privileged,
             tty,
         )
