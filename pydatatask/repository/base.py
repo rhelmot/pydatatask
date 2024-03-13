52fd0b619c546112b0d00a2a92ffebc0278d91da
Lukas Dresel <Lukas-Dresel@users.noreply.github.com>
2024-03-13 01:43:18 -0700

    better error message

--- pydatatask/repository/base.py
+++ pydatatask/repository/base.py
@@ -542,7 +542,7 @@ class FileRepository(FileRepositoryBase, BlobRepository):  # BlobFileRepository?
     @job_getter
     async def open(self, job, mode="r"):
         if not self.is_valid_job_id(job):
-            raise KeyError(job)
+            raise KeyError(f"Invalid job id {job} for {self}")
         return aiofiles.open(self.fullpath(job), mode)  # type: ignore
 
     async def delete(self, job, /):
