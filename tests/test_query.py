b48fa73221bd89eb15fb3b0014ade3ac2365f1b8
Audrey Dutcher <audrey@rhelmot.io>
2024-03-29 12:42:03 -0700

    add testcase for query repositories

---
+++ tests/test_query.py
@@ -0,0 +1,31 @@
+from typing import cast
+import pathlib
+import unittest
+
+from pydatatask.cli.lock import default_allocators_temp
+from pydatatask.repository.base import MetadataRepository
+from pydatatask.staging import Dispatcher, PipelineStaging
+from pydatatask.task import ProcessTask
+
+test_root = pathlib.Path(__file__).parent
+
+
+class TestQuery(unittest.IsolatedAsyncioTestCase):
+    async def test_query(self):
+        staging = PipelineStaging(test_root / "content" / "query" / "pipeline.yaml")
+        allocated = staging.allocate(default_allocators_temp, Dispatcher("TempLinux", {"app": "test_streaming"}))
+        pipeline = allocated.instantiate()
+        # pipeline.settings(debug_trace=True)
+        task0 = cast(ProcessTask, pipeline.tasks["task0"])
+        task1 = cast(ProcessTask, pipeline.tasks["task1"])
+        inputs = cast(MetadataRepository, task0.links["output"].repo)
+        filtered = cast(MetadataRepository, task1.links["input"].repo)
+
+        async with pipeline:
+            await inputs.dump("1", {"works": False, "is": "one"})
+            await inputs.dump("2", {"works": True, "is": "two"})
+            assert await filtered.info_all() == {"2": {"works": True, "is": "two"}}
+
+
+if __name__ == "__main__":
+    unittest.main()
