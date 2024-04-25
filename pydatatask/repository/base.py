0af2c9463831d879f5434c2297372f4a06cf5cc2
Audrey Dutcher <audrey@rhelmot.io>
2024-04-25 00:40:38 -0700

    Don't mind me, just debugging

--- pydatatask/repository/base.py
+++ pydatatask/repository/base.py
@@ -537,6 +537,7 @@ class FileRepositoryBase(Repository, ABC):
                 yield name[: -len(self.extension) if self.extension else None]
 
     async def validate(self):
+        print("WE R MAKING", self.basedir)
         self.basedir.mkdir(exist_ok=True, parents=True)
         if not os.access(self.basedir, os.W_OK):
             raise PermissionError(f"Cannot write to {self.basedir}")
